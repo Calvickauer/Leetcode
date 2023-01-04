6ad70a81c4d363e3df40ba707436136a16885443
calvickauer <calvickauer@gmail.com>
2023-01-04 01:50:52 -0800

    RunningSum Done

---
+++ leetcode.js
@@ -0,0 +1,18 @@
+// let nums = [1,2,3,4];
+
+// var runningSum = function(nums) {
+//     let answer = [];
+//     for (i = 0; i < nums.length; i++){
+//         if(answer.length === 0){
+//             answer.push(nums[i]);
+//         } else {
+//             answer.push(nums[i] + answer[i - 1])
+//         }
+//     }
+//     return answer;
+// };
+
+// console.log(runningSum(nums));
+
+// works
+
